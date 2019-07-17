345ec6d59e0337eaadbd36a471efdf41c35cf630
Jamey Hampton <jhampton@agrilyst.com>
2019-07-17 11:27:28 -0400

    Updates to User (based on changes to the actual API) (#4)
    * updated to handle the new user api calls
    
    * added user tests
    
    * accidentally lost the expiration changes michael made

--- lib/artemis_api/client.rb
+++ lib/artemis_api/client.rb
@@ -1,7 +1,8 @@
 module ArtemisApi
   class Client
     require 'oauth2'
-    attr_reader :options, :objects, :access_token, :refresh_token, :oauth_client, :oauth_token, :expires_in, :created_at, :expires_at
+    attr_reader :options, :objects, :access_token, :refresh_token, :oauth_client,
+                :oauth_token, :expires_in, :created_at, :expires_at
 
     def initialize(access_token, refresh_token, expires_in, created_at, options = {})
       options[:app_id] ||= ENV['ARTEMIS_OAUTH_APP_ID']
@@ -16,48 +17,48 @@ module ArtemisApi
 
       @oauth_client = OAuth2::Client.new(@options[:app_id], @options[:app_secret], site: @options[:base_uri])
       @oauth_token = OAuth2::AccessToken.from_hash(
-                      oauth_client, 
+                      oauth_client,
                       {access_token: @access_token,
                        refresh_token: @refresh_token,
                        created_at: @created_at,
                        expires_in: @expires_in,
                        expires_at: @expires_at})
-      @objects = {} 
+      @objects = {}
     end
 
-    def find_one(type, id, force = false)
+    def find_one(type, id, facility_id: nil, include: nil, force: false)
       obj = get_record(type, id)
       if !obj || force
-        response = @oauth_token.get("#{@options[:base_uri]}/api/v3/#{type}/#{id}")
-        if response.status == 200
-          json = JSON.parse(response.body)
-          obj = store_record(type, id, json['data'])
+        refresh if @oauth_token.expired?
 
-          #if json['included']
-          #  json['included'].each do |included_obj|
-          #    store_record(included_obj['type'], included_obj['id'], included_obj)
-          #  end
-          #end
-        end
+        url = if facility_id
+                "#{@options[:base_uri]}/api/v3/facilities/#{facility_id}/#{type}/#{id}"
+              else
+                "#{@options[:base_uri]}/api/v3/#{type}/#{id}"
+              end
+        url = "#{url}?include=#{include}" if include
+
+        response = @oauth_token.get(url)
+        obj = process_response(response, type) if response.status == 200
       end
       obj
     end
 
-    def find_all(type, params = nil)
+    def find_all(type, facility_id: nil, include: nil, params: nil)
       records = []
-      response = @oauth_token.get("#{@options[:base_uri]}/api/v3/#{type}")
-      if response.status == 200
-        json = JSON.parse(response.body)
-        json['data'].each do |obj|
-          record = store_record(type, obj['id'], obj)
-          records << record
-        end
+      refresh if @oauth_token.expired?
 
-        #if json['included']
-        #  json['included'].each do |included_obj|
-        #    store_record(included_obj['type'], included_obj['id'], included_obj)
-        #  end
-        #end
+      url = if facility_id
+              "#{@options[:base_uri]}/api/v3/facilities/#{facility_id}/#{type}"
+            else
+              "#{@options[:base_uri]}/api/v3/#{type}"
+            end
+      url = "#{url}?include=#{include}" if include
+      url = "#{url}?params=#{params}" if params
+
+      response = @oauth_token.get(url)
+      if response.status == 200
+        records = process_array(response, type, records)
       end
       records
     end
@@ -73,6 +74,33 @@ module ArtemisApi
 
     def refresh
       @oauth_token = @oauth_token.refresh!
+    end
+
+    private
+
+    def process_response(response, type)
+      json = JSON.parse(response.body)
+      obj = store_record(type, json['data']['id'].to_i, json['data'])
+      process_included_objects(json['included']) if json['included']
+
+      obj
+    end
+
+    def process_array(response, type, records)
+      json = JSON.parse(response.body)
+      json['data'].each do |obj|
+        record = store_record(type, obj['id'], obj)
+        records << record
+      end
+      process_included_objects(json['included']) if json['included']
+
+      records
+    end
+
+    def process_included_objects(included_array)
+      included_array.each do |included_obj|
+        store_record(included_obj['type'], included_obj['id'], included_obj)
+      end
     end
   end
 end
