2a761fd088d3c5c47a95fc2acc2b33c8f2f232cb
Jamie Hampton <jhampton@agrilyst.com>
2019-07-31 16:42:53 -0400

    added filters option to batches

--- lib/artemis_api/batch.rb
+++ lib/artemis_api/batch.rb
@@ -6,8 +6,8 @@ module ArtemisApi
       client.find_one(self.json_type, id, facility_id: facility_id, include: include, force: force)
     end
 
-    def self.find_all(facility_id, client, include: nil)
-      client.find_all(self.json_type, facility_id: facility_id, include: include)
+    def self.find_all(facility_id, client, include: nil, filters: nil)
+      client.find_all(self.json_type, facility_id: facility_id, include: include, filters: filters)
     end
 
     def actions
