1b17f1b63aa961bae9dbbb326c7c9ef473bba2c2
Jamie Hampton <jhampton@agrilyst.com>
2019-09-04 15:35:03 -0400

    added models for seeding and harvest units

--- lib/artemis_api.rb
+++ lib/artemis_api.rb
@@ -1,14 +1,16 @@
-require "artemis_api/client"
-require "artemis_api/model"
-require "artemis_api/facility"
-require "artemis_api/user"
-require "artemis_api/version"
-require "artemis_api/zone"
-require "artemis_api/organization"
-require "artemis_api/batch"
-require "artemis_api/completion"
-require "artemis_api/discard"
-require "artemis_api/harvest"
+require 'artemis_api/client'
+require 'artemis_api/model'
+require 'artemis_api/facility'
+require 'artemis_api/user'
+require 'artemis_api/version'
+require 'artemis_api/zone'
+require 'artemis_api/organization'
+require 'artemis_api/batch'
+require 'artemis_api/completion'
+require 'artemis_api/discard'
+require 'artemis_api/harvest'
+require 'artemis_api/seeding_units'
+require 'artemis_api/harvest_units'
 
 module ArtemisApi
   # Your code goes here...
