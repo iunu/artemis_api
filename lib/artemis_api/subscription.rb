de425f9b3dbecfe5ea2d17f566d8d0a1ddac5bf1
Jamie Hampton <jhampton@agrilyst.com>
2019-10-15 16:53:51 -0400

    first shot at a post call

--- lib/artemis_api/subscription.rb
+++ lib/artemis_api/subscription.rb
@@ -9,5 +9,20 @@ module ArtemisApi
     def self.find_all(facility_id, client, include: nil, filters: nil)
       client.find_all(self.json_type, facility_id: facility_id, include: include, filters: filters)
     end
+
+    def self.create(facility_id, subject, destination, client)
+      client.oauth_token.refresh! if client.oauth_token.expired?
+
+      url = "#{client.options[:base_uri]}/api/v3/facilities/#{facility_id}/subscriptions"
+      params = { body:
+                  { subject: subject,
+                    destination: destination } }
+
+      response = client.oauth_token.post(url, params)
+
+      # TODO: do we need to do something other than this?
+      # does the response include the created object? can we store it in the objects hash?
+      response.status == 200 ? true : false
+    end
   end
 end
