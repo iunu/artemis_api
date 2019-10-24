2516eac78846638bd4ac39ec0005f5d47423e315
Jamie Hampton <jhampton@agrilyst.com>
2019-10-24 13:06:40 -0400

    using a new method to build a uri to get the appropriate url

--- lib/artemis_api/client.rb
+++ lib/artemis_api/client.rb
@@ -27,12 +27,16 @@ module ArtemisApi
       if !obj || force
         refresh if @oauth_token.expired?
 
-        url = if facility_id
-                "#{@options[:base_uri]}/api/v3/facilities/#{facility_id}/#{type}/#{id}"
-              else
-                "#{@options[:base_uri]}/api/v3/#{type}/#{id}"
-              end
-        url = "#{url}?include=#{include}" if include
+        path = if facility_id
+                 "/api/v3/facilities/#{facility_id}/#{type}/#{id}"
+               else
+                 "/api/v3/#{type}/#{id}"
+               end
+
+        query = ''
+        query += "include=#{include}" if include
+
+        url = build_url(path: path, query: query)
 
         response = @oauth_token.get(url)
         obj = process_response(response, type) if response.status == 200
@@ -44,25 +48,30 @@ module ArtemisApi
       records = []
       refresh if @oauth_token.expired?
 
-      url = if facility_id && batch_id
-              "#{@options[:base_uri]}/api/v3/facilities/#{facility_id}/batches/#{batch_id}/#{type}"
-            elsif facility_id && batch_id.nil?
-              "#{@options[:base_uri]}/api/v3/facilities/#{facility_id}/#{type}"
-            else
-              "#{@options[:base_uri]}/api/v3/#{type}"
-            end
+      path = if facility_id && batch_id
+               "/api/v3/facilities/#{facility_id}/batches/#{batch_id}/#{type}"
+             elsif facility_id && batch_id.nil?
+               "/api/v3/facilities/#{facility_id}/#{type}"
+             else
+               "/api/v3/#{type}"
+             end
 
-      url = "#{url}?include=#{include}" if include
-      if filters
-        formatted_filters = format_filters(filters)
-        url = (include) ? "#{url}&#{formatted_filters}" : "#{url}?#{formatted_filters}"
-      end
+      query = ''
+      query += "include=#{include}" if include
+      query += format_filters(filters) if filters
+
+      url = build_url(path: path, query: query)
 
       response = @oauth_token.get(url)
       if response.status == 200
         records = process_array(response, type, records)
       end
       records
+    end
+
+    def build_url(path:, query: nil)
+      uri = URI::Generic.build(path: path, query: query)
+      @options[:base_uri] + uri.to_s
     end
 
     def store_record(type, id, data)
@@ -72,6 +81,10 @@ module ArtemisApi
 
     def get_record(type, id)
       @objects[type]&.[](id.to_i)
+    end
+
+    def remove_record(type, id)
+      @objects[type].delete(id.to_i)
     end
 
     def refresh
